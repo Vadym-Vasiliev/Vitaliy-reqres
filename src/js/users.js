a9e3786cd6864b4e98febeb15d645ded02f2e6c9
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-11-08 10:00:02 +0200

    add fix

--- src/js/users.js
+++ src/js/users.js
@@ -1,5 +1,8 @@
 const content = document.querySelector(".main__list");
 const moreBtn = document.querySelector(".main__btn");
+const itemCards = document.querySelectorAll(".main__item");
+console.log(itemCards);
+
 let currentPage = 1;
 
 async function loadUsers(page) {
@@ -21,13 +24,15 @@ function getUsers(users) {
   users.data.forEach((user) => {
     const template = `
     <div class="main__item">
-      <img class="main__img" src="${user.avatar}">
+      
       <div class="main__content-user">
-        <div class="main__user">Id: ${user.id}</div>
-        <div class="main__user">Email: ${user.email}</div>
         <div class="main__user">First_name: ${user.first_name}</div>
         <div class="main__user">Last_name: ${user.last_name}</div>
+        <div class="main__user">Email: ${user.email}</div>
+        <div class="main__user main__ids" id="user">Id: ${user.id}</div>
       </div>
+      <img class="main__img" src="${user.avatar}">
+      
     </div>`;
 
     content.innerHTML += template;
